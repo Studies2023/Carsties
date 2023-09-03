a82e635210faf0a56e4b62c56a6c8b0b9a0b9a6f
Igor Fialkov <github@shtilkind.net>
2023-09-03 16:36:31 -0700

    End of section 2.

---
+++ src/AuctionService/Data/AuctionDbContext.cs
@@ -0,0 +1,13 @@
+﻿using AuctionService.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuctionService.Data;
+
+public sealed class AuctionDbContext : DbContext
+{
+    public DbSet<Auction> Auctions { get; set; }
+    public AuctionDbContext(DbContextOptions options) 
+        : base(options)
+    {
+    }
+}
